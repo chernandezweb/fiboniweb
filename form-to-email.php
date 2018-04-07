d78bc464459684f9522217cbf41ec01877b23c8e
Carlos Hernandez <chernandezweb@gmail.com>
2018-04-07 16:25:54 -0400

    Formulaire Update

---
+++ form-to-email.php
@@ -0,0 +1,51 @@
+<?php
+$errors = '';
+$myemail = 'chernandezweb@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['contactName'])  ||
+   empty($_POST['contactEmail']) ||
+   empty($_POST['contactMessage']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['contactName'];
+$email_address = $_POST['contactEmail'];
+$message = $_POST['contactMessage'];
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+	$to = $myemail;
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
+
+	$headers = "From: $myemail\n";
+	$headers .= "Reply-To: $email_address";
+
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: thank-you.html');
+}
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
